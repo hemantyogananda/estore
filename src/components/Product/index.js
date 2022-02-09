e28247da3666ba6c1306420b45cbf62362034933
Annie <prashantyogananda@gmail.com>
2022-02-09 08:50:32 +0530

    Added Redux in eStore for dynamic data

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import { useSelector,useDispatch } from 'react-redux';
+import * as actions from "../../redux/actions/product";
 import "./_product.scss";
 
-const products = [
-    require("../../assets/img/shop/shop-1.jpg"),
-    require("../../assets/img/shop/shop-2.jpg"),
-    require("../../assets/img/shop/shop-3.jpg"),
-    require("../../assets/img/shop/shop-4.jpg"),
-    require("../../assets/img/shop/shop-5.jpg"),
-]
 
-const Product = () => (
+const Product = () => {
+    const dispatch = useDispatch();
+    const {product:{products}} = useSelector((obj)=>obj);
+    useEffect(()=>{
+        dispatch(actions.getProducts());
+    })
+    return(
     <div className="row">
         {products.map((item, index) => (
 
@@ -17,10 +18,10 @@ const Product = () => (
             <div className="col-lg-4 col-md-6">
                 <div className="product__item">
                     <div className="product__item__pic">
-                        <img className="product__item__pic" src={item} />
+                        <img className="product__item__pic" src={item.imageSrc} />
                         <ul className="product__hover">
                             <li>
-                                <a href={item}>
+                                <a href={item.imageSrc}>
                                     <span className="fa fa-arrows-alt" />
                                 </a>
                             </li>
@@ -28,7 +29,7 @@ const Product = () => (
                     </div>
                     <div className="product__item__text">
                         <h6>
-                            <a href="#">Furry Hooded</a>
+                            <a href="#">{item.name}</a>
                         </h6>
                         <div className="rating">
                             <i className="fa fa-star" />
@@ -37,12 +38,12 @@ const Product = () => (
                             <i className="fa fa-star" />
                             <i className="fa fa-star" />
                         </div>
-                        <div className="product__price">$590.0</div>
+                        <div className="product__price">{item.price}</div>
                     </div>
                 </div>
             </div>
         ))}
     </div>
-)
+)}
 
 export default Product;
