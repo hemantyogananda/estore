3e2cc6e34b3d3b6cf5c7a399643d41d31f5c845c
Annie <prashantyogananda@gmail.com>
2022-02-12 10:24:06 +0530

    fixing conflicts

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -19,38 +19,6 @@ const Product = () => {
         dispatch(actions.getProducts());
     }, [])
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-            <div className="col-lg-4 col-md-6">
-                <div className="product__item">
-                    <div className="product__item__pic">
-                        <img className="product__item__pic" src={item.imageSrc} />
-                        <ul className="product__hover">
-                            <li>
-                                <a href={item.imageSrc}>
-                                    <span className="fa fa-arrows-alt" />
-                                </a>
-                            </li>
-                            <li>
-                                <a href={null}>
-                                    <span className="fa fa-shopping-cart" />
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="product__item__text">
-                        <h6>
-                            <a href="#">{item.name}</a>
-                        </h6>
-                        <div className="rating">
-                            <i className="fa fa-star" />
-                            <i className="fa fa-star" />
-                            <i className="fa fa-star" />
-                            <i className="fa fa-star" />
-                            <i className="fa fa-star" />
-=======
-=======
->>>>>>> f18f15cdc8b180e6fa36ff857be42e0d37e31f89
     
     return (
         <div className="row">
@@ -79,10 +47,6 @@ const Product = () => {
                                 <i className="fa fa-star" />
                             </div>
                             <div className="product__price">{item.price}</div>
-<<<<<<< HEAD
->>>>>>> completed Database for eStore
-=======
->>>>>>> f18f15cdc8b180e6fa36ff857be42e0d37e31f89
                         </div>
                     </div>
                 </div>
